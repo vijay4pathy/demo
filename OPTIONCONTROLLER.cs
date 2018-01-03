977d32d415602920785648dd671afa214096e2ba
vijay4pathy <35061178+vijay4pathy@users.noreply.github.com>
2018-01-03 14:49:17 +0530

    Add files via upload
    test

---
+++ OPTIONCONTROLLER.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRStandardAssets.Utils;
+
+
+public class OPTIONCONTROLLER : MonoBehaviour {
+	[SerializeField]
+	VRStandardAssets.Utils.VRInteractiveItem VRInteract;
+	public delegate void ClickAction();
+	public static event ClickAction OnClicked;
+	public GameObject CurrentObject;
+	public GameObject[] OtherObjects;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnEnable()
+	{
+		VRInteract.OnClick += ShowOptions;
+
+
+
+	}
+
+
+
+	void ShowOptions()
+	{
+		
+
+			foreach (GameObject G in OtherObjects) {
+			
+				G.SetActive (false);
+			}
+	//	if (CurrentObject.activeSelf != true) {
+			CurrentObject.SetActive (true);
+		//}
+		//else
+			//CurrentObject.SetActive (false);
+		
+
+
+
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
