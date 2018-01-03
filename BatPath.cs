abd8db70ef6edfb1c9b61b66e7b0a59d43280932
vijay4pathy <35061178+vijay4pathy@users.noreply.github.com>
2018-01-03 15:01:45 +0530

    Add files via upload

---
+++ BatPath.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatPath : MonoBehaviour
+{
+	public GameObject onText;
+	public GameObject offText;
+
+	private bool display_path;
+
+	void Start ()
+	{
+		display_path = StaticVariables.bat_path;
+		if (display_path) 
+		{
+			onText.SetActive (true);
+			offText.SetActive (false);
+		}
+		else
+		{
+			offText.SetActive (true);
+			onText.SetActive (false);
+		}
+	}
+
+	public void On_Off()
+	{
+		display_path = StaticVariables.Path_on_off ();
+		if (display_path) 
+		{
+			onText.SetActive (true);
+			offText.SetActive (false);
+		}
+		else
+		{
+			offText.SetActive (true);
+			onText.SetActive (false);
+		}
+	}
+}
